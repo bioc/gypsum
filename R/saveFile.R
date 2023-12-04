21e65b2897183a7fe168ad8d1a84aac3f105c136
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-12-04 12:56:05 -0800

    Avoid saving the file again if it has already been cached.

--- R/saveFile.R
+++ R/saveFile.R
@@ -8,9 +8,12 @@
 #' @param path String containing the relative path to the file inside the version.
 #' @param destination String containing the destination path for the file.
 #' If \code{NULL}, a path inside the \code{\link{cacheDirectory}} is used.
-#' @param check Whether to check if the file exists before attempting a download.
+#' @param precheck Whether to check if the file exists before attempting a download.
 #' This may be omitted if the file is known to exist.
 #' @param config Configuration object for the S3 bucket, see \code{\link{publicS3Config}} for details.
+#' @param overwrite Logical scalar indicating whether to overwrite an existing file at \code{destination}.
+#' If \code{FALSE} and \code{destination} exists, the download is skipped.
+#' Defaults to \code{!is.null(destination)}, so no extra downloads are performed if the file is already cached.
 #' 
 #' @return The file is downloaded to the local file system.
 #' The destination file path is invisibly returned.
@@ -28,32 +31,38 @@
 #' 
 #' @export
 #' @importFrom aws.s3 object_exists save_object
-saveFile <- function(project, asset, version, path, destination=NULL, config=publicS3Config(), check=TRUE) {
-    args <- list(
-        object=paste0(project, "/", asset, "/", version, "/", path),
-        bucket=config$bucket, 
-        key=config$key, 
-        secret=config$secret, 
-        base_url=sub("^http[s]://", "", config$endpoint), 
-        region=""
-    )
-
-    if (check && !do.call(object_exists, args)) {
-        stop(
-            "no object exists at '", path, 
-            "' for version '", version, 
-            "' of asset '", asset, 
-            "' of project '", project, "'"
-        )
-    }
-
+saveFile <- function(project, asset, version, path, destination=NULL, config=publicS3Config(), precheck=TRUE, overwrite=NULL) {
     if (is.null(destination)) {
+        if (is.null(overwrite)) {
+            overwrite <- FALSE
+        }
         destination <- file.path(cacheDirectory(), "bucket", project, asset, version, path)
         dir.create(dirname(destination), recursive=TRUE, showWarnings=FALSE)
     }
 
-    args$file <- destination
-    args$parse_response <- FALSE
-    do.call(save_object, args)
+    if (!file.exists(destination) || overwrite) {
+        args <- list(
+            object=paste0(project, "/", asset, "/", version, "/", path),
+            bucket=config$bucket, 
+            key=config$key, 
+            secret=config$secret, 
+            base_url=sub("^http[s]://", "", config$endpoint), 
+            region=""
+        )
+
+        if (precheck && !do.call(object_exists, args)) {
+            stop(
+                "no object exists at '", path, 
+                "' for version '", version, 
+                "' of asset '", asset, 
+                "' of project '", project, "'"
+            )
+        }
+
+        args$file <- destination
+        args$parse_response <- FALSE
+        do.call(save_object, args)
+    }
+
     invisible(destination)
 }
