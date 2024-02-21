568e5057aa98db1f5990cf7d9a45b25903ef05e7
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-02-20 22:33:40 -0800

    Updated the metadata fetchers to the new URL.
    Also just include the file extension in the name for simplicity.

--- R/fetchMetadataSchema.R
+++ R/fetchMetadataSchema.R
@@ -1,13 +1,12 @@
 #' Fetch a metadata schema
 #'
 #' Fetch a JSON schema file for metadata to be inserted into a SQLite database
-#' (see \url{https://github.com/ArtifactDB/gypsum-to-sqlite}).
+#' (see \url{https://github.com/ArtifactDB/bioconductor-metadata-index}).
 #' Each SQLite database is created from metadata files uploaded to the gypsum backend,
 #' so clients uploading objects to be incorporated into the database should validate their metadata against the corresponding JSON schema.
 #'
 #' @param name String containing the name of the schema.
-#' This can be the name of any JSON schema file in \url{https://github.com/ArtifactDB/gypsum-to-sqlite/tree/master/schemas},
-#' after removing the \code{.json} suffix.
+#' This can be the name of any JSON schema file published at \url{https://github.com/ArtifactDB/bioconductor-metadata-index}.
 #' @param cache String containing the cache directory.
 #' If \code{NULL}, no caching is performed.
 #' @param overwrite Logical scalar indicating whether to overwrite an existing file in \code{cache}, if one is present.
@@ -25,13 +24,14 @@
 #' fetchMetadataSchema()
 #' @export
 #' @importFrom filelock lock unlock
-fetchMetadataSchema <- function(name="bioconductor", cache=cacheDirectory(), overwrite=FALSE) {
+fetchMetadataSchema <- function(name="bioconductor/v1.json", cache=cacheDirectory(), overwrite=FALSE) {
     if (is.null(cache)) {
         cache.path <- tempfile(fileext=".json")
     } else {
         cache.dir <- file.path(cache, "schemas")
-        dir.create(cache.dir, showWarnings=FALSE, recursive=TRUE)
-        cache.path <- file.path(cache.dir, paste0(name, ".json"))
+        cache.path <- file.path(cache.dir, name)
+        dir.create(dirname(cache.path), showWarnings=FALSE, recursive=TRUE)
+
         if (file.exists(cache.path) && !overwrite) {
             flck <- lock(paste0(cache.path, ".LOCK"), exclusive=FALSE)
             unlock(flck)
@@ -39,7 +39,7 @@ fetchMetadataSchema <- function(name="bioconductor", cache=cacheDirectory(), ove
         }
     }
 
-    url <- paste0("https://raw.githubusercontent.com/ArtifactDB/gypsum-to-sqlite/master/schemas/", name, ".json")
+    url <- paste0("https://artifactdb.github.io/bioconductor-metadata-index/", name)
 
     flck <- lock(paste0(cache.path, ".LOCK"))
     on.exit(unlock(flck))
