b7f4206782ba4f16bd391a4e29d778335dc83a83
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2024-02-24 14:08:37 -0800

    Added text search on metadata based on tokens in the SQLite file. (#2)
    This uses a dedicated 'gypsum.search.clause' class to allow users to assemble
    complex queries with standard syntax. We also allow developers to just obtain
    the WHERE filter directly if they want to assemble their own SQL queries.

---
+++ R/searchMetadataText.R
@@ -0,0 +1,269 @@
+#' Text search on the metadata database
+#'
+#' Perform a text search on a SQLite database containing metadata from the gypsum backend.
+#' This is based on a precomputed tokenization of all string properties in each metadata document;
+#' see \url{https://github.com/ArtifactDB/bioconductor-metadata-index} for details.
+#'
+#' @param query Character vector specifying the query to execute.
+#' Alternatively, a \code{gypsum.search.object} produced by \code{defineTextQuery}.
+#' @param path String containing a path to a SQLite file, usually obtained via \code{\link{fetchMetadataDatabase}}.
+#' @param latest Logical scalar indicating whether to only search for matches within the latest version of each asset.
+#' @param include.metadata Logical scalar indicating whether metadata should be returned.
+#' @param pid.name String containing the name/alias of the column of the \code{paths} table that contains the path ID.
+#' @param text String containing the text to query on.
+#' This will be automatically tokenized, see Details.
+#' @param field String specifying the name of the metadata field in which to search for \code{text}.
+#' If \code{NULL}, the search is performed on all available metadata fields.
+#' @param partial Logical scalar indicating whether \code{text} contains SQLite wildcards (\code{\%}, \code{_}) for a partial search.
+#' If \code{TRUE}, the wildcards are preserved during tokenization.
+#'
+#' @return 
+#' For \code{searchMetadataText}, a data frame specifying the contaning the search results.
+#' \itemize{
+#' \item The \code{project}, \code{asset} and \code{version} columns contain the identity of the version with matching metadata.
+#' \item The \code{path} column contains the path to the metadata document within the version prefix.
+#' \item If \code{include.metadata=TRUE}, a \code{metadata} column is present with the nested metadata for each match.
+#' \item If \code{latest=TRUE}, a \code{latest} column is present indicating whether the matching version is the latest for its asset.
+#' Otherwise, only the latest version is returned.
+#' }
+#'
+#' For \code{searchMetadataTextFilter}, a list containing \code{where}, a string can be directly used as a WHERE filter condition in a SQL SELECT statement;
+#' and \code{parameters}, the parameter bindings to be used in \code{where}.
+#' The return value may also be \code{NULL} if the query has no well-defined filter.
+#'
+#' For \code{defineTextQuery}, a \code{gypsum.search.clause} object that can be used in \code{|} and \code{&} to create more complex queries involving multiple text clauses.
+#'
+#' @author Aaron Lun
+#'
+#' @details
+#' Each string is tokenized by converting it to lower case and splitting it on characters that are not Unicode letters/numbers or a dash.
+#' We currently do not remove diacritics so these will need to be converted to ASCII by the user. 
+#' If a text query involves only non-letter/number/dash characters, the filter will not be well-defined and will be ignored when constructing SQL statements.
+#'
+#' For convenience, a non-empty character vector may be used in \code{query}.
+#' A character vector of length 1 is treated as shorthand for a text query with default arguments in \code{defineTextQuery}.
+#' A character vector of length greater than 1 is treated as shorthand for an AND operation on default text queries for each of the individual strings.
+#' 
+#' @seealso
+#' \code{\link{fetchMetadataDatabase}}, to download and cache the database files.
+#'
+#' \url{https://github.com/ArtifactDB/bioconductor-metadata-index}, for details on the SQLite file contents and table structure.
+#' 
+#' @examples
+#' path <- fetchMetadataDatabase()
+#' searchMetadataText(path, c("mouse", "brain"), include.metadata=FALSE)
+#'
+#' # Now for a slightly more complex query:
+#' is.mouse <- defineTextQuery("10090", field="taxonomy_id")
+#' query <- (defineTextQuery("brain") | defineTextQuery("pancreas")) & is.mouse
+#' searchMetadataText(path, query, include.metadata=FALSE)
+#'
+#' # Throwing in some wildcards.
+#' has.neuro <- defineTextQuery("Neuro%", partial=TRUE)
+#' searchMetadataText(path, has.neuro, include.metadata=FALSE)
+#'
+#' @aliases
+#' gypsum.search.clause
+#' Ops.gypsum.search.clause
+#' @export
+searchMetadataText <- function(path, query, latest=TRUE, include.metadata=TRUE) {
+    where <- searchMetadataTextFilter(query)
+    cond <- where$where
+    params <- where$parameters
+
+    conn <- DBI::dbConnect(RSQLite::SQLite(), path)
+    on.exit(DBI::dbDisconnect(conn))
+
+    stmt <- "SELECT versions.project AS project, versions.asset AS asset, versions.version AS version, path";
+    if (include.metadata) {
+        stmt <- paste0(stmt, ", json_extract(metadata, '$') AS metadata")
+    }
+    if (!latest) {
+        stmt <- paste0(stmt, ", versions.latest AS latest")
+    }
+    stmt <- paste0(stmt, " FROM paths LEFT JOIN versions ON paths.vid = versions.vid")
+
+    if (latest) {
+        cond <- c(cond, "versions.latest = 1")
+    }
+    if (length(cond)) {
+        stmt <- paste0(stmt, " WHERE ", paste(cond, collapse=" AND "))
+    }
+
+    if (is.null(params)) {
+        everything <- DBI::dbGetQuery(conn, stmt)
+    } else {
+        everything <- DBI::dbGetQuery(conn, stmt, params=params)
+    }
+    if (include.metadata) {
+        everything$metadata <- lapply(everything$metadata, fromJSON, simplifyVector=FALSE)
+    }
+    everything
+}
+
+#' @export
+#' @rdname searchMetadataText
+defineTextQuery <- function(text, field=NULL, partial=FALSE) {
+    output <- list(type="text", text=text, field=field, partial=partial)
+    class(output) <- "gypsum.search.clause"
+    output
+}
+
+#' @export
+Ops.gypsum.search.clause <- function(e1, e2) {
+    if (.Generic == "&") {
+        output <- list(type="and", children=list(e1, e2))
+    } else if (.Generic == "|") {
+        output <- list(type="or", children=list(e1, e2))
+    } else {
+        stop("unsupported generic '", .Generic, "' for 'gypsum.search.clause'")
+    }
+    class(output) <- "gypsum.search.clause"
+    output
+}
+
+#' @export
+#' @rdname searchMetadataText
+searchMetadataTextFilter <- function(query, pid.name = 'paths.pid') {
+    query <- sanitize_query(query)
+    if (is.null(query)) {
+        return(NULL)
+    }
+
+    env <- new.env()
+    env$parameters <- list()
+    cond <- build_query(query, pid.name, env)
+    list(where=cond, parameters=env$parameters)
+}
+
+sanitize_query <- function(query) {
+    if (is.character(query)) {
+        if (length(query) > 1) {
+            query <- list(type="and", children=lapply(query, defineTextQuery))
+            class(query) <- "gypsum.search.clause"
+        } else if (length(query) == 1L) {
+            query <- defineTextQuery(query)
+        } else {
+            stop("character vector must be non-empty")
+        }
+    }
+
+    qt <- query$type
+    if (qt != "text") {
+        rechildren <- lapply(query$children, sanitize_query)
+
+        keep <- !vapply(rechildren, is.null, FALSE)
+        if (!any(keep)) {
+            return(NULL)
+        }
+        if (!all(keep)) {
+            rechildren <- rechildren[keep]
+        }
+        if (length(rechildren) == 1L) {
+            return(rechildren[[1]])
+        }
+
+        # Collapse AND/ORs into a single layer to reduce nesting of subqueries.
+        can.merge <- vapply(rechildren, function(x) x$type, "") == qt
+        if (any(can.merge)) {
+            grandchildren <- lapply(rechildren[can.merge], function(x) x$children)
+            rechildren <- c(unlist(grandchildren, recursive=FALSE), rechildren[!can.merge])
+        }
+
+        output <- list(type=qt, children=rechildren)
+        class(output) <- "gypsum.search.clause"
+        return(output)
+    }
+
+    extras <- ""
+    if (isTRUE(query$partial)) {
+        extras <- "%_"
+    }
+
+    text <- strsplit(tolower(query$text), sprintf("[^\\p{N}\\p{L}\\p{Co}%s-]", extras), perl=TRUE)[[1]]
+    keep <- vapply(text, nchar, 0L) > 0L
+    if (!any(keep)) {
+        return(NULL)
+    }
+
+    children <- lapply(text[keep], defineTextQuery, field=query$field, partial=isTRUE(query$partial))
+    if (length(children) == 1L) {
+        return(children[[1]])
+    }
+
+    output <- list(type="and", children=children)
+    class(output) <- "gypsum.search.clause"
+    output 
+}
+
+add_query_parameter <- function(env, value) {
+    newname <- paste0("p", length(env$parameters))
+    env$parameters[[newname]] <- value
+    newname
+}
+
+build_query <- function(query, name, env) {
+    qt <- query$type
+    if (qt == "text") {
+        nt <- add_query_parameter(env, query$text)
+        if (isTRUE(query$partial)) {
+            match.str <- paste0("tokens.token LIKE :", nt) 
+        } else {
+            match.str <- paste0("tokens.token = :", nt) 
+        }
+
+        field <- query$field
+        if (!is.null(field)) {
+            nf <- add_query_parameter(env, field)
+            return(sprintf("%s IN (SELECT pid from links LEFT JOIN tokens ON tokens.tid = links.tid LEFT JOIN fields ON fields.fid = links.fid WHERE %s AND fields.field = :%s)", name, match.str, nf))
+        } else {
+            return(sprintf("%s IN (SELECT pid from links LEFT JOIN tokens ON tokens.tid = links.tid WHERE %s)", name, match.str))
+        }
+    }
+
+    if (qt == "and") {
+        out <- lapply(query$children, build_query, name=name, env=env)
+        return(paste0("(", paste(out, collapse=" AND "), ")"))
+    }
+
+    is.text <- vapply(query$children, function(x) query$type, "") == "text"
+    out <- character(0)
+
+    # Collapse text children into a single subquery.
+    if (any(is.text)) {
+        textual <- query$children[is.text]
+        needs.field <- FALSE
+
+        for (i in seq_along(textual)) {
+            current <- textual[[i]]
+            nt <- add_query_parameter(env, current[[1]])
+            if (isTRUE(current$partial)) {
+                match.str <- paste0("tokens.token LIKE :", nt) 
+            } else {
+                match.str <- paste0("tokens.token = :", nt) 
+            }
+
+            field <- current$field
+            if (is.null(field)) {
+                textual[[i]] <- match.str
+            } else {
+                nf <- add_query_parameter(env, field)
+                textual[[i]] <- sprintf("(%s AND fields.field = :%s)", match.str, nf)
+                needs.field <- TRUE
+            }
+        }
+
+        textual <- paste(unlist(textual), collapse=" OR ")
+        if (needs.field) {
+            out <- c(out, sprintf("%s IN (SELECT pid from links LEFT JOIN tokens ON tokens.tid = links.tid LEFT JOIN fields ON fields.fid = links.fid WHERE %s)", name, textual))
+        } else {
+            out <- c(out, sprintf("%s IN (SELECT pid from links LEFT JOIN tokens ON tokens.tid = links.tid WHERE %s)", name, textual))
+        }
+    }
+
+    # All non-text children to be processed as separate subqueries, I'm afraid.
+    if (!all(is.text)) {
+        out <- c(out, vapply(query$children[!is.text], build_query, name=name, env=env, ""))
+    }
+    paste0("(", paste(out, collapse=" OR "), ")")
+}
---
+++ tests/testthat/test-searchMetadataText.R
@@ -0,0 +1,156 @@
+# This tests the searchMetadataText function.
+# library(testthat); library(gypsum); source("test-searchMetadataText.R")
+
+library(DBI)
+library(RSQLite)
+
+tmp <- tempfile(fileext=".sqlite3")
+(function() {
+    conn <- dbConnect(SQLite(), tmp)
+    on.exit(dbDisconnect(conn))
+
+    dbWriteTable(conn, "versions", data.frame(
+        vid = 1:3,
+        project = "foo",
+        asset = "bar",
+        version = as.character(1:3),
+        latest = c(FALSE, FALSE, TRUE)
+    ))
+
+    metadata <- list(
+        list(first_name="mikoto", last_name="misaka", school="tokiwadai", ability="railgun", gender="female", comment="rank 3"),
+        list(first_name="mitsuko", last_name="kongou", school="tokiwadai", ability="aerohand", gender="female"),
+        list(first_name="kuroko", last_name="shirai", school="tokiwadai", ability="teleport", gender="female", affiliation="judgement"),
+        list(first_name="misaki", last_name="shokuhou", school="tokiwadai", ability="mental out", gender="female", comment="rank 5"),
+        list(first_name="ruiko", last_name="saten", school="sakugawa", gender="female"),
+        list(first_name="kazari", last_name="uiharu", school="sakugawa", gender="female", affiliation="judgement"),
+        list(first_name="accelerator", ability="vector manipulation", gender="male", comment="rank 1")
+    )
+
+    dbWriteTable(conn, "paths", data.frame(
+        pid = seq_along(metadata),
+        vid = rep(1:3, length.out=length(metadata)),
+        path = paste0(vapply(metadata, function(x) x$first_name, ""), ".txt"),
+        metadata = vapply(metadata, jsonlite::toJSON, auto_unbox=TRUE, "")
+    ))
+
+    all.tokens <- unlist(strsplit(unique(unlist(metadata, use.names=FALSE)), " "))
+    dbWriteTable(conn, "tokens", data.frame(tid = seq_along(all.tokens), token = all.tokens))
+
+    all.fields <- unique(unlist(lapply(metadata, names)))
+    dbWriteTable(conn, "fields", data.frame(fid = seq_along(all.fields), field = all.fields))
+
+    links <- list(pid = integer(0), fid = integer(0), tid = integer(0))
+    for (i in seq_along(metadata)) {
+        my.fields <- names(metadata[[i]])
+        my.tokens <- lapply(metadata[[i]], function(x) unique(strsplit(x, " ")[[1]]))
+        my.fields <- rep(my.fields, lengths(my.tokens))
+        my.tokens <- unlist(my.tokens, use.names=FALSE)
+        links$pid <- c(links$pid, rep(i, length(my.tokens)))
+        links$fid <- c(links$fid, match(my.fields, all.fields))
+        links$tid <- c(links$tid, match(my.tokens, all.tokens))
+    }
+    dbWriteTable(conn, "links", data.frame(links))
+})()
+
+test_that("searchMetadataText works for text searches", {
+    out <- searchMetadataText(tmp, c("mikoto"), include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, "mikoto.txt")
+
+    # Tokenization works correctly.
+    out <- searchMetadataText(tmp, c(" kuroko "), include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, c("kuroko.txt"))
+    out <- searchMetadataText(tmp, c("TOKIWADAI"), include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, c("mikoto.txt", "mitsuko.txt", "kuroko.txt", "misaki.txt"))
+
+    # Partial matching works correctly.
+    query <- defineTextQuery("Mi%", partial=TRUE)
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, c("mikoto.txt", "mitsuko.txt", "misaki.txt"))
+
+    # Field-specific matching works correctly.
+    query <- defineTextQuery("sa%", partial=TRUE, field="last_name")
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, c("ruiko.txt"))
+})
+
+test_that("searchMetadataText works for AND searches", {
+    # AND automatically happens upon tokenization.
+    out <- searchMetadataText(tmp, c("sakugawa judgement"), include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, "kazari.txt")
+
+    # We can also be more explicit.
+    query <- defineTextQuery("rank") & defineTextQuery("male")
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, "accelerator.txt")
+
+    # Nested ANDs are handled properly.
+    query <- (defineTextQuery("s%", partial=TRUE) & defineTextQuery("tokiwadai")) & defineTextQuery("judgement")
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, "kuroko.txt")
+})
+
+test_that("searchMetadataText works for OR searches", {
+    query <- defineTextQuery("uiharu") | defineTextQuery("rank")
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, c("mikoto.txt", "misaki.txt", "kazari.txt", "accelerator.txt"))
+
+    # ORs work correctly with partial matches.
+    query <- defineTextQuery("judgement") | defineTextQuery("Mi%", partial=TRUE)
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, c("mikoto.txt", "mitsuko.txt", "kuroko.txt", "misaki.txt", "kazari.txt"))
+
+    # ORs work correctly with field matches.
+    query <- defineTextQuery("mi%", partial=TRUE, field="last_name") | defineTextQuery("judgement")
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, c("mikoto.txt", "kuroko.txt", "kazari.txt"))
+
+    # Nested ORs are collapsed properly.
+    query <- (defineTextQuery("teleport") | defineTextQuery("aerohand")) | defineTextQuery("mental")
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, c("mitsuko.txt", "kuroko.txt", "misaki.txt"))
+})
+
+test_that("searchMetadataText works with combined AND and OR searches", {
+    # OR that contains an AND.
+    query <- (defineTextQuery("judgement") & defineTextQuery("sakugawa")) | defineTextQuery("aerohand") | defineTextQuery("vector")
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, c("mitsuko.txt", "kazari.txt", "accelerator.txt"))
+
+    # OR that contains multiple ANDs.
+    query <- (defineTextQuery("judgement") & defineTextQuery("sakugawa")) | (defineTextQuery("female") & defineTextQuery("rank"))
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, c("mikoto.txt", "misaki.txt", "kazari.txt"))
+
+    # AND that contains an OR.
+    query <- defineTextQuery("rank") & (defineTextQuery("shokuhou") | defineTextQuery("kongou"))
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, c("misaki.txt"))
+
+    # AND that contains multiple ORs.
+    query <- (defineTextQuery("rank") | defineTextQuery("judgement")) & (defineTextQuery("male") | defineTextQuery("teleport"))
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(out$path, c("kuroko.txt", "accelerator.txt"))
+})
+
+test_that("searchMetadataText works with ill-defined filters", {
+    # We return everything.
+    out <- searchMetadataText(tmp, "     ", include.metadata=FALSE, latest=FALSE)
+    expect_identical(nrow(out), 7L)
+
+    # Ill-defined filters are ignored in boolean operations.
+    query <- defineTextQuery("female") & defineTextQuery("    ")
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(nrow(out), 6L)
+
+    query <- defineTextQuery("male") | defineTextQuery("    ")
+    out <- searchMetadataText(tmp, query, include.metadata=FALSE, latest=FALSE)
+    expect_identical(nrow(out), 1L)
+})
+
+test_that("searchMetadataText respects the other output options", {
+    out <- searchMetadataText(tmp, c("female"))
+    expect_identical(out$path, c("kuroko.txt", "kazari.txt"))
+    expect_identical(out$path, paste0(vapply(out$metadata, function(x) x$first_name, ""), ".txt"))
+})
+
